075fc89cdb1f28c9a00814c79f5fd313c8709019
VolodymyrPlastun <vplastun8@gmail.com>
2022-09-29 12:52:06 +0300

    Add empty library component

--- src/components/EmptyLibrary/EmptyLibrary.jsx
+++ src/components/EmptyLibrary/EmptyLibrary.jsx
@@ -1,48 +1,72 @@
-// import { useSelector } from "react-redux";
-// import { useState } from 'react';
-import { List, ListItem, Box, Title, StepText, StepTexIcon, StepDescr, StepDescrIcon } from './EmptyLibrary.styled';
 import icons from '../../images/svg/icons.svg';
+import {
+    List,
+    BookName,
+    ArrowBackIcon,
+    MobileContainer,
+    Btn,
+    ListItem,
+    Box,
+    Title,
+    StepText,
+    StepTexIcon,
+    StepDescr,
+    StepDescrIcon,
+} from './EmptyLibrary.styled';
 
 export const EmptyLibrary = () => {
-    // const isOpen = useSelector(state => state.showModal);
-    //  const [showModal, setShowModal] = useState(false);
-    // console.log(showModal)
-    //     const toggleModal = () => {
-    //         setShowModal(!showModal);
-    //     };
     return (
+        <>
+        <MobileContainer>
+        
+        <ArrowBackIcon width="24" height="12">
+               <use href={`${icons}#icon-arrowBack`} />
+           </ArrowBackIcon>
+           
+           <BookName>Назва книги</BookName>
+           </MobileContainer>
         <Box>
-            <List>
-                <ListItem>
-                    <Title>Крок 1.</Title>
-                    <StepText>
-                        
+                <List>
+                    <ListItem>
+                        <Title>Крок 1.</Title>
+                        <StepText>
                             <StepTexIcon width="22" height="17">
                                 <use href={`${icons}#icon-book`} />
                             </StepTexIcon>
-                        Створіть особисту бібліотеку
-                    </StepText>
-                    <StepDescr>
-                    <StepDescrIcon width="10" height="12">
-                                <use href={`${icons}#icon-perpendicularArrow`} />
+                            Створіть особисту бібліотеку
+                        </StepText>
+                        <StepDescr>
+                            <StepDescrIcon width="10" height="12">
+                                <use
+                                    href={`${icons}#icon-perpendicularArrow`}
+                                />
                             </StepDescrIcon>
-                     Додайте до неї книжки, які маєте намір прочитати.</StepDescr>
-                </ListItem>
-                <ListItem>
-                    <Title>Крок 2.</Title>
-                    <StepText>
+                            Додайте до неї книжки, які маєте намір прочитати.
+                        </StepDescr>
+                    </ListItem>
+                    <ListItem>
+                        <Title>Крок 2.</Title>
+                        <StepText>
                             <StepTexIcon width="15" height="17">
                                 <use href={`${icons}#icon-flag`} />
-                            </StepTexIcon> Сформуйте своє перше тренування
-                            </StepText>
-                    <StepDescr>
-                    <StepDescrIcon width="10" height="12">
-                                <use href={`${icons}#icon-perpendicularArrow`} />
+                            </StepTexIcon>{' '}
+                            Сформуйте своє перше тренування
+                        </StepText>
+                        <StepDescr>
+                            <StepDescrIcon width="10" height="12">
+                                <use
+                                    href={`${icons}#icon-perpendicularArrow`}
+                                />
                             </StepDescrIcon>
-                        Визначте ціль, оберіть період, розпочинайте тренування.
-                    </StepDescr>
-                </ListItem>
-            </List>
+                            Визначте ціль, оберіть період, розпочинайте
+                            тренування.
+                        </StepDescr>
+                    </ListItem>
+                </List>
+                <Btn variant={'accent'} type="button">
+                    Ок
+                </Btn>
         </Box>
+        </>
     );
 };
--- src/components/EmptyLibrary/ModalBtn.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export const ModalBtn = ({toggleModal}) => {
-    return (
-           <button type="button" onClick={toggleModal}>Modal</button>
-    )
-}
