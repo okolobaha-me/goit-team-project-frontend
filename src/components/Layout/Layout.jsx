98e474275e5a3c1b952bf8f283905f545478914b
VolodymyrPlastun <vplastun8@gmail.com>
2022-09-30 17:23:27 +0300

    Add info component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
+import Info from '../../pages/Info/Info';
 
 export const Layout = () => {
     return (
         <>
             <Header />
+            <Info />
             <Outlet />
         </>
     );
