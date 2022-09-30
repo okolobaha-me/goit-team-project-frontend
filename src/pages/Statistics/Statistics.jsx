e69b5067ec1bb6c0a5b91ee4ac55e8eadbd987c1
Igor1997Klimenko <Igorek.klimuch.97@gmail.com>
2022-09-30 15:06:49 +0300

    добавил линии статистики

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,9 +1,92 @@
+import {
+    LineChart,
+    Line,
+    XAxis,
+    Tooltip,
+    CartesianGrid,
+  } from "recharts";
+import { BooksDay, NumberDay, Span, StatisticsHome, TextStats } from "./Statistics.styled";
+const data = [
+    {
+      uv: 4000,
+      pv: 2400,
+      amt: 2400
+    },
+    {
+      uv: 3000,
+      pv: 1398,
+      amt: 2210
+    },
+    {
+      uv: 2000,
+      pv: 6800,
+      amt: 2290
+    },
+    {
+      uv: 2780,
+      pv: 3908,
+      amt: 2000
+    },
+    {
+      uv: 1890,
+      pv: 4800,
+      amt: 2181
+    },
+    {
+      uv: 2390,
+      pv: 3800,
+      amt: 2500
+    },
+    {
+      uv: 3490,
+      pv: 4300,
+      amt: 2100
+    }
+  ];
 const Statistics = () => {
     return (
-        <>
-            <h1>Statistics</h1>
+      <>       
+        <StatisticsHome>
+        <BooksDay>
+          <TextStats>Кількість сторінок</TextStats>
+          <Span> / </Span>
+          <TextStats> день </TextStats>
+          <NumberDay>0</NumberDay>
+        </BooksDay>
+        <LineChart
+        width={810}
+        height={245}
+        data={data}
+        >
+      <CartesianGrid  vertical horizontal={false}/>
+      <XAxis 
+        dataKey="name"
+        label={{ value: "Time", position: "insideBottomRight", offset: 0 }}
+        stroke="#B1B5C2" 
+        tickLine={false}
+      />
+        <Tooltip />
+      <Line
+        type="monotone"
+        dataKey="pv"
+        stroke="#091E3F"
+        strokeWidth={2}
+        activeDot={{ r: 5 }}
+      />
+      <Line type="monotone"
+        dataKey="uv"
+        stroke="#FF6B08"
+        strokeWidth={2}
+       />
+    </LineChart>
+    </StatisticsHome>
         </>
     );
 };
 
 export default Statistics;
+
+
+
+
+
---
+++ src/pages/Statistics/Statistics.styled.jsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled'
+
+export const StatisticsHome = styled.div({
+    margin: '39px 16px 51px 32px',
+    padding: '35px 77px 30px 40px',
+    width: 'min-content',
+    height: 'auto',
+    boxShadow: '0px 1px 3px rgb(9 30 63 / 25%)'
+  })
+
+  export const BooksDay = styled.div({
+    margin: '0 0 20px 5px',
+    display: 'flex',
+    alignItems: 'center',
+  })
+
+  export const TextStats = styled.span({
+    fontWeight: '500',
+    fontSize: '12px',
+    lineHeight: '15px',
+    textTransform: 'uppercase',
+    color: '#242A37'
+  })
+
+  export const Span = styled.span({
+    margin: '0 3px',
+  })
+
+  export const NumberDay = styled.span({
+    fontWeight: '600',
+    fontSize: '12px',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    color: '#242A37',
+    background: '#F5F7FA',
+    display: 'block',
+    margin: '0 10px',
+    padding: '5px',
+  })
