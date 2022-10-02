398bb7e94c72dc7397b23819399d03a15556f706
SergiyRybin <serhiobax@gmail.com>
2022-10-02 13:03:43 +0300

    remove from Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
-import Timer from '../Timer/Timer';
 
 export const Layout = () => {
     return (
         <>
             <Header />
-            <Timer />
             <Outlet />
         </>
     );
