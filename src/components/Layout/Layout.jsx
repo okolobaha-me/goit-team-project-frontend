841e2062fd564fc2c9bf3b2198e8324507c1f6ac
SergiyRybin <serhiobax@gmail.com>
2022-10-02 11:18:51 +0300

    Added double timer with adaptive design. Placed at <Layout/> just to visualization.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header/Header';
+import Timer from '../Timer/Timer';
 
 export const Layout = () => {
     return (
         <>
             <Header />
+            <Timer />
             <Outlet />
         </>
     );
---
+++ src/components/Timer/Timer.styled.jsx
@@ -0,0 +1,78 @@
+import styled from '@emotion/styled';
+import { colors, fonts, mq } from '../../baseStyles';
+
+export const TaimerMain = styled.div`
+    display: flex;
+    align-items: baseline;
+    justify-content: center;
+    width: 290px;
+    height: 60px;
+    background-color: ${colors.whiteBg};
+    margin-top: 9px;
+    padding: 0 38px;
+    box-shadow: 4px 4px 8px rgba(36, 42, 55, 0.15);
+`;
+
+export const TimerTitle = styled.div`
+    font-family: ${fonts.main};
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 17px;
+    text-align: center;
+    margin-top: 24px;
+    color: ${colors.darkGrayText};
+    ${mq.mobile} {
+        &:first-of-type {
+            margin-top: 20px;
+        }
+    }
+`;
+
+export const TimerDays = styled.span`
+    font-family: ${fonts.main};
+    font-weight: 500;
+    font-size: 10px;
+    line-height: 12px;
+    text-align: center;
+    color: ${colors.darkGrayText};
+`;
+
+export const TimerNumber = styled.span`
+    font-family: ${fonts.openSans};
+    font-weight: 700;
+    font-size: 25px;
+    line-height: 38px;
+    color: ${colors.mainText};
+`;
+
+export const TimerList = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 0 10px;
+    &:first-of-type {
+        margin-left: 0;
+    }
+    &:last-of-type {
+        margin-right: 0;
+    }
+`;
+
+export const TaimerAlign = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    ${mq.notTablet} {
+        flex-direction: column;
+    }
+`;
+
+export const TimerContainer = styled.div`
+    margin-right: 20px;
+    &:last-of-type {
+        margin-right: 0;
+    }
+    ${mq.notTablet} {
+        margin-right: 0;
+    }
+`;
